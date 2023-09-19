f99832099d5024a01e067591b11f40c95c7e41a9
sanieni6 <luissanchezzamora23@gmail.com>
2023-09-19 12:00:25 -0500

    Configure tailwindcss and set basic styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
+    "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blueColor: '#2C7EF8',
+        greenColor: '#59AE43',
+        redColor: '#F82C2C',
+        backHeader: '#252F3D',
+
+      },
+    },
   },
   plugins: [],
 }
